f51fd5b6feb21326fd14911c85e59c73ae7ac813
Andrew Williams <andy@tensixtyone.com>
2011-03-02 22:35:32 +0000

    Added README and basic setup

---
+++ README.rst
@@ -0,0 +1,7 @@
+----------
+python-ts3
+----------
+
+python-ts3 is a abstraction library around the Teamspeak 3 ServerQuery API. It 
+allows native access to the ServerQuery API with most of the formatting 
+headaches avoided.
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name = "python-ts3",
+	version = "0.01",
+	description = "TS3 ServerQuery library for Python",
+	author = "Andrew Willaims",
+	author_email = "andy@tensixtyone.com",
+	url = "https://github.com/nikdoof/python-ts3/",
+	py_modules = ['ts3',],
+)
