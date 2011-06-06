92203c7de2c6c0c19bf0019fdb15d2ebe44b735e
Andrew Williams <andy@tensixtyone.com>
2011-06-06 09:25:42 +0100

    Added classifiers for pypi

--- setup.py
+++ setup.py
@@ -4,10 +4,18 @@ from distutils.core import setup
 from ts3 import __version__
  
 setup(name = "python-ts3",
-	version = __version__,
-	description = "TS3 ServerQuery library for Python",
-	author = "Andrew Willaims",
-	author_email = "andy@tensixtyone.com",
-	url = "https://github.com/nikdoof/python-ts3/",
-	packages = ['ts3',],
+    version = __version__,
+    description = "TS3 ServerQuery library for Python",
+    author = "Andrew Willaims",
+    author_email = "andy@tensixtyone.com",
+    url = "https://github.com/nikdoof/python-ts3/",
+    packages = ['ts3',],
+
+    classifiers = [
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet',
+        'Topic :: Communications',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+    ]
 )
