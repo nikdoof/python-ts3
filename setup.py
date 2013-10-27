7bb9dbf21e00cdb645ccdd77975d56465a3e00cc
Andrew <andy@tensixtyone.com>
2013-10-27 17:05:40 +0000

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
  
-from distutils.core import setup
+from setuptools import setup
 from ts3 import __version__
  
-setup(name = "python-ts3",
-    version = __version__,
-    description = "TS3 ServerQuery library for Python",
-    author = "Andrew Willaims",
-    author_email = "andy@tensixtyone.com",
-    url = "https://github.com/nikdoof/python-ts3/",
-    keywords = "teamspeak ts3 voice serverquery teamspeak3",
-    packages = ['ts3',],
-    scripts = ['examples/gents3privkey.py'],
+setup(
+    name="python-ts3",
+    version=__version__,
+    description="TS3 ServerQuery library for Python",
+    author="Andrew Willaims",
+    author_email="andy@tensixtyone.com",
+    url="https://github.com/nikdoof/python-ts3/",
+    keywords="teamspeak ts3 voice serverquery teamspeak3",
+    packages=['ts3'],
+    scripts=['examples/gents3privkey.py'],
+    test_suite='ts3.test.suite',
 
-    classifiers = [
+    classifiers=[
         'License :: OSI Approved :: BSD License',
         'Topic :: Internet',
         'Topic :: Communications',
